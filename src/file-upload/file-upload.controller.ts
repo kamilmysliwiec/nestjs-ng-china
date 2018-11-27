f6c8f933e395809142ab1e4045272e2af849f1d7
zchapple <zack_chapple@ultimatesoftware.com>
2018-11-27 09:57:55 -0500

    feat(core): adding more swagger docs

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -2,10 +2,14 @@ import {Controller, FileInterceptor, FilesInterceptor, HttpStatus, Post, Res, Up
 import * as path from 'path';
 import * as fs from 'fs';
 import {Response} from 'express';
+import {ApiConsumes, ApiImplicitFile, ApiResponse} from '@nestjs/swagger';
 
 @Controller('file-upload')
 export class FileUploadController {
   @Post('single')
+  @ApiConsumes('multipart/form-data')
+  @ApiImplicitFile({name: 'file', required: true, description: 'User Uploaded File'})
+  @ApiResponse({status: HttpStatus.ACCEPTED, description: 'The File has been saved'})
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file, @Res() res: Response) {
     fs.createWriteStream(path.join(`static_files/${file.originalname}`));
@@ -13,9 +17,12 @@ export class FileUploadController {
   }
 
   @Post('multiple')
+  @ApiConsumes('multipart/form-data')
+  @ApiImplicitFile({name: 'file upload', required: true, description: 'User Uploaded File'})
+  @ApiResponse({status: HttpStatus.ACCEPTED, description: 'The Files have been saved'})
   @UseInterceptors(FilesInterceptor('files'))
-  uploadFiles(@UploadedFiles() files,  @Res() res: Response) {
-    //TODO: This should handle duplicate file errors
+  uploadFiles(@UploadedFiles() files, @Res() res: Response) {
+    // TODO: This should handle duplicate file errors
     files.forEach((file) => {
       fs.createWriteStream(path.join(`static_files/${file.originalname}`));
     });
